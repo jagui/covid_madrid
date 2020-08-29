2b9ddd1b9a4be44c4f08544e1ae007673825818e
Juan Agüí <juan@juanagui.com>
2020-08-29 19:04:08 +0200

    splits weekly and daily graphs to provide a clearer picture

--- app.py
+++ app.py
@@ -20,9 +20,20 @@ confirmed_cases_14d_key = "casos_confirmados_ultimos_14dias"
 tia_14d_key = "tasa_incidencia_acumulada_ultimos_14dias"
 confirmed_cases_dayone_key = "casos_confirmados_totales"
 tia_dayone_key = "tasa_incidencia_acumulada_total"
-cam_confirmed_cases_dayone_key = "madrid_casos_confirmados_totales"
-cam_confirmed_cases_14d_key = "madrid_casos_confirmados_ultimos_14dias"
-new_cases_dayone_key = "new_cases"
+cam_confirmed_cases_dayone_key_pre_july_2nd = (
+    "madrid_casos_confirmados_totales_pre_july_2nd"
+)
+cam_confirmed_cases_14d_key_pre_july_2nd = (
+    "madrid_casos_confirmados_ultimos_14dias_pre_july_2nd"
+)
+cam_confirmed_cases_dayone_key_post_july_2nd = (
+    "madrid_casos_confirmados_totales_post_july_2nd"
+)
+cam_confirmed_cases_14d_key_post_july_2nd = (
+    "madrid_casos_confirmados_ultimos_14dias_post_july_2nd"
+)
+new_cases_dayone_key_pre_july_2nd = "new_cases_pre_july_2nd"
+new_cases_dayone_key_post_july_2nd = "new_cases_post_july_2nd"
 top_10_zsb_14d_key = "top_10_zsb_14d"
 zone_key = "zona_basica_salud"
 date_key = "fecha_informe"
@@ -36,80 +47,89 @@ figures_keys = [
 cam_zone_key = "cam"
 
 try:
-    local_csv = os.environ["LOCAL_CSV"]
+    local_csv = os.environ["LOCAL_CSV"] == "1"
 except KeyError:
     local_csv = False
 
+source = "./data/covid19_tia_zonas_basicas_salud.csv"
+big_df_pre_july_2nd = (
+    pd.read_csv(
+        source, sep=";", encoding="latin_1", decimal=",", parse_dates=[date_key],
+    )
+    .fillna(0.0)
+    .drop_duplicates([date_key, zone_key], inplace=False)
+)
 
 ssl._create_default_https_context = ssl._create_unverified_context
-source = (
-    "./data/covid19_tia_zonas_basicas_salud.csv"
-    if local_csv
-    else "https://datos.comunidad.madrid/catalogo/dataset/b3d55e40-8263-4c0b-827d-2bb23b5e7bab/resource/b7b9edb4-0c70-47d3-9c64-8c4913830a24/download/covid19_tia_zonas_basicas_salud.csv"
-)
-big_df_pre_july_2nd = pd.read_csv(
-    source, sep=";", encoding="latin_1", decimal=",", parse_dates=[date_key],
-).fillna(0.0)
-
-
 source = (
     "./data/covid19_tia_zonas_basicas_salud_s.csv"
     if local_csv
     else "https://datos.comunidad.madrid/catalogo/dataset/b3d55e40-8263-4c0b-827d-2bb23b5e7bab/resource/43708c23-2b77-48fd-9986-fa97691a2d59/download/covid19_tia_zonas_basicas_salud_s.csv"
 )
-big_df_post_july_2nd = pd.read_csv(
-    source, sep=";", encoding="latin_1", decimal=",", parse_dates=[date_key],
-).fillna(0.0)
 
-big_df = pd.concat([big_df_pre_july_2nd, big_df_post_july_2nd])
+big_df_post_july_2nd = (
+    pd.read_csv(
+        source, sep=";", encoding="latin_1", decimal=",", parse_dates=[date_key],
+    )
+    .fillna(0.0)
+    .drop_duplicates([date_key, zone_key], inplace=False)
+)
 
-big_df.drop_duplicates([date_key, zone_key], inplace=True)
+mask = big_df_post_july_2nd[date_key] >= "2020-07-07"
 
-all_zsbs = big_df[[zone_key]].drop_duplicates()
+big_df_post_july_2nd = big_df_post_july_2nd.loc[mask]
 
-max_date = big_df[date_key].max()
+all_zsbs = big_df_post_july_2nd[[zone_key]].drop_duplicates()
 
-figure_dfs = {}
+max_date = big_df_post_july_2nd[date_key].max()
+
+figure_dfs_pre_july_2nd = {}
 for key in figures_keys:
-    df = big_df.pivot(index=date_key, columns=zone_key, values=key)
+    df = big_df_pre_july_2nd.pivot(index=date_key, columns=zone_key, values=key)
     df.loc[:, cam_zone_key] = df.sum(axis=1)
-    figure_dfs[key] = df
+    figure_dfs_pre_july_2nd[key] = df
+
+figure_dfs_post_july_2nd = {}
+for key in figures_keys:
+    df = big_df_post_july_2nd.pivot(index=date_key, columns=zone_key, values=key)
+    df.loc[:, cam_zone_key] = df.sum(axis=1)
+    figure_dfs_post_july_2nd[key] = df
 
 pd.options.plotting.backend = "plotly"
 
 dfs = {}
 
-all_zsb_14d_df = (
-    big_df[big_df[date_key] == max_date][
+top_10_zsb_14d_df = (
+    big_df_post_july_2nd[big_df_post_july_2nd[date_key] == max_date][
         [zone_key, tia_14d_key, confirmed_cases_14d_key]
     ]
     .set_index(zone_key)
     .sort_values(by=[tia_14d_key, confirmed_cases_14d_key], ascending=False)
-)
-
-top_10_zsb_14d_df = all_zsb_14d_df.head(10)
+).head(10)
 
 default_zones_df = top_10_zsb_14d_df.head(4)
 
-dfs.update(
-    {top_10_zsb_14d_key: top_10_zsb_14d_df[[tia_14d_key, confirmed_cases_14d_key]]}
-)
-
-dfs.update(
-    {cam_confirmed_cases_14d_key: figure_dfs[confirmed_cases_14d_key][cam_zone_key]}
-)
-
-dfs.update(
-    {
-        cam_confirmed_cases_dayone_key: figure_dfs[confirmed_cases_dayone_key][
-            cam_zone_key
-        ]
-    }
-)
-
-dfs.update(
-    {new_cases_dayone_key: figure_dfs[confirmed_cases_dayone_key][cam_zone_key].diff()}
-)
+dfs = {
+    top_10_zsb_14d_key: top_10_zsb_14d_df[[tia_14d_key, confirmed_cases_14d_key]],
+    cam_confirmed_cases_14d_key_pre_july_2nd: figure_dfs_pre_july_2nd[
+        confirmed_cases_14d_key
+    ][cam_zone_key],
+    cam_confirmed_cases_dayone_key_pre_july_2nd: figure_dfs_pre_july_2nd[
+        confirmed_cases_dayone_key
+    ][cam_zone_key],
+    cam_confirmed_cases_14d_key_post_july_2nd: figure_dfs_post_july_2nd[
+        confirmed_cases_14d_key
+    ][cam_zone_key],
+    cam_confirmed_cases_dayone_key_post_july_2nd: figure_dfs_post_july_2nd[
+        confirmed_cases_dayone_key
+    ][cam_zone_key],
+    new_cases_dayone_key_pre_july_2nd: figure_dfs_pre_july_2nd[
+        confirmed_cases_dayone_key
+    ][cam_zone_key].diff(),
+    new_cases_dayone_key_post_july_2nd: figure_dfs_post_july_2nd[
+        confirmed_cases_dayone_key
+    ][cam_zone_key].diff(),
+}
 
 app = dash.Dash(
     "Covid Madrid by Tesla Cool Lab",
@@ -125,11 +145,15 @@ def update_zones(zones):
         return []
     dfs = {}
 
-    for key, df in figure_dfs.items():
+    for key, df in figure_dfs_post_july_2nd.items():
         dfs.update({key: df[zones]})
 
     dfs.update(
-        {new_cases_dayone_key: figure_dfs[confirmed_cases_dayone_key][zones].diff()}
+        {
+            new_cases_dayone_key_post_july_2nd: figure_dfs_post_july_2nd[
+                confirmed_cases_dayone_key
+            ][zones].diff()
+        }
     )
 
     figures = [
@@ -156,7 +180,11 @@ def update_zones(zones):
             lg=6,
         ),
         dbc.Col(
-            dcc.Graph(figure=named_fig(new_cases_dayone_key, dfs, "Nuevos casos",)),
+            dcc.Graph(
+                figure=named_fig(
+                    new_cases_dayone_key_post_july_2nd, dfs, "Nuevos casos",
+                )
+            ),
             lg=6,
         ),
         dbc.Col(
@@ -188,14 +216,6 @@ app.layout = dbc.Container(
         ),
         html.P(
             [
-                "Hasta el 1 de julio de 2020 los registros de datos correspondientes a cada fecha de informe se han añadido diariamente. ",
-                html.Em(
-                    "A partir del 2 de julio de 2020 la actualización pasa a ser semanal."
-                ),
-            ],
-        ),
-        html.P(
-            [
                 "Mapa disponible ",
                 html.A(
                     "aquí",
@@ -203,8 +223,18 @@ app.layout = dbc.Container(
                 ),
             ]
         ),
-        html.H2("Comunidad de Madrid"),
+        html.H2("Comunidad de Madrid: desde 2 de julio"),
         html.Hr(),
+        html.P(
+            [
+                "A partir del día 2 julio de 2020 la Comunidad de Madrid publica sus informes de forma semanal. Las siguientes gráficas se nutren de los datos publicados en este ",
+                html.A(
+                    "enlace",
+                    href="https://datos.comunidad.madrid/catalogo/dataset/covid19_tia_zonas_basicas_salud/resource/43708c23-2b77-48fd-9986-fa97691a2d59",
+                ),
+                ".",
+            ]
+        ),
         dbc.Row(
             [
                 dbc.Col(
@@ -221,7 +251,7 @@ app.layout = dbc.Container(
                 dbc.Col(
                     dcc.Graph(
                         figure=named_fig(
-                            cam_confirmed_cases_14d_key,
+                            cam_confirmed_cases_14d_key_post_july_2nd,
                             dfs,
                             "Comunidad de Madrid: casos confirmados 14 días",
                         )
@@ -231,7 +261,7 @@ app.layout = dbc.Container(
                 dbc.Col(
                     dcc.Graph(
                         figure=named_fig(
-                            new_cases_dayone_key,
+                            new_cases_dayone_key_post_july_2nd,
                             dfs,
                             "Comunidad de Madrid: nuevos casos",
                         ),
@@ -241,7 +271,7 @@ app.layout = dbc.Container(
                 dbc.Col(
                     dcc.Graph(
                         figure=named_fig(
-                            cam_confirmed_cases_dayone_key,
+                            cam_confirmed_cases_dayone_key_post_july_2nd,
                             dfs,
                             "Comunidad de Madrid: casos confirmados totales",
                         ),
@@ -250,7 +280,7 @@ app.layout = dbc.Container(
                 ),
             ],
         ),
-        html.H2("Por zonas sanitarias básicas"),
+        html.H2("Por zonas sanitarias básicas desde el 2 de julio"),
         html.Hr(),
         html.Label("Seleccione zonas sanitarias básicas"),
         dcc.Dropdown(
@@ -263,6 +293,55 @@ app.layout = dbc.Container(
             multi=True,
         ),
         dbc.Row(id="figures"),
+        html.H2("Comunidad de Madrid hasta 1 de julio"),
+        html.Hr(),
+        html.P(
+            [
+                "Hasta el día 1 de julio de 2020 la Comunidad de Madrid publicaba sus datos diariamente. Proporcionamos las gráficas basadas en ",
+                html.A(
+                    "estos datos",
+                    href="https://datos.comunidad.madrid/catalogo/dataset/covid19_tia_zonas_basicas_salud/resource/b7b9edb4-0c70-47d3-9c64-8c4913830a24",
+                ),
+                " históricos por separado de los datos semanales. ",
+                html.Em(
+                    "A partir del 2 de julio de 2020 la actualización pasa a ser semanal."
+                ),
+            ],
+        ),
+        dbc.Row(
+            [
+                dbc.Col(
+                    dcc.Graph(
+                        figure=named_fig(
+                            cam_confirmed_cases_14d_key_pre_july_2nd,
+                            dfs,
+                            "Comunidad de Madrid: casos confirmados 14 días",
+                        )
+                    ),
+                    lg=6,
+                ),
+                dbc.Col(
+                    dcc.Graph(
+                        figure=named_fig(
+                            new_cases_dayone_key_pre_july_2nd,
+                            dfs,
+                            "Comunidad de Madrid: nuevos casos",
+                        ),
+                    ),
+                    lg=6,
+                ),
+                dbc.Col(
+                    dcc.Graph(
+                        figure=named_fig(
+                            cam_confirmed_cases_dayone_key_pre_july_2nd,
+                            dfs,
+                            "Comunidad de Madrid: casos confirmados totales",
+                        ),
+                    ),
+                    lg=6,
+                ),
+            ],
+        ),
         html.Hr(),
         html.P(
             [
